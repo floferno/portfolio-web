86da5afaf8ab944543c581deb491139bfbb44e14
Olfia R. W. <58562757+floferno@users.noreply.github.com>
2024-09-11 01:04:24 +0800

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -1,10 +1,8 @@
 const userData = {
   githubUsername: "floferno",
   name: "Olfia Rachmi",
-  designation: "Frontend Developer",
+  designation: "Software Engineer",
   avatarUrl: "/avatar.jpg",
-  email: "olfiarachmi@gmail.com",
-  // phone: "+62 812 3456 7890",
   address: "Bali, Indonesia",
   projects: [
     {
@@ -41,58 +39,50 @@ const userData = {
   ],
   about: {
     title:
-      "I am a web (*cough* a little bit of a weeb too *cough*) developer, coding teacher, and digital illustrator. I love to create things that live on the internet that *hopefully* please the eye. My goals are to always build products that provide pixel-perfect, excellent performance experiences, and share stuff I know to others",
+      "I am a web (*cough* a little bit of a weeb too *cough*) developer, coding teacher, and digital illustrator.",
     description: [
-      `I've been interested in programming since high school and self-teaching it in my spare time as a mere hobby but I had taken a break for several years due to the lack of motivation and time.`,
-      `In 2020, I was at the peak of feeling stuck and anxious of my career hence I decided to learn programming more intensively and seriously while still doing my daytime job by enrolling in Hacktiv8's Fullstack Web Development Bootcamp, Freecodecamp, some other online courses such as Web Programming Unpas, BuildWithAngga, Coursera, and Udemy. I also found myself a supportive mentor in MOFON Indonesia who helps me deepen my programming understanding.`,
-      `After what I've learned so far, I've decided to focus more on being a Front-End Developer as my starting career in IT industry. I'm so pumped up to learn, grow, and put my skills to greater use in the future! 🔥`
+      `I love to create things that *hopefully* please the eye. My goals are to always build products that provide pixel-perfect, excellent performance experiences, and share with others`
     ],
     currentProject: "Art Magazine",
     currentProjectUrl: "https://art-gallery-floferno.vercel.app/",
   },
   experience: [
-    {
-      title: "Full-time Front-end Developer",
-      company: "Venatronics LLc",
+      {
+      title: "Full-time Web Developer",
+      company: "Semantu",
+      year: "2023 - present",
+      companyLink: "https://www.semantu.com/",
+      desc: "- Building product-based and "
+    },
+     {
+      title: "Part-Time Coding Teacher",
+      company: "Timedoor",
       year: "2022 - present",
+      companyLink: "https://timedooracademy.com/",
+      desc: "- Teaching coding for kids and teenagers with Scratch, Javascript, Phaser, and React Native by building games, websites and mobile apps, also teaching essential knowledge related to technology in general. - Making reports to parents about their kids' learning progress or needs.",
+    },
+     {
+      title: "Full-time Web Developer",
+      company: "Venatronics LLc",
+      year: "2022 - 2023",
       companyLink: "https://www.venatronics.com/",
       desc: "- Building responsive company websites using React"
     },
     {
       title: "Part-Time Coding Teacher",
       company: "Kodland",
-      year: "2022 - present",
+      year: "2022 - 2023",
       companyLink: "https://www.kodland.org/",
       desc: "- Teaching coding for kids with Scratch by building games. - Making reports to parents about their kids' learning progress or needs.",
     },
     {
-      title: "Part-Time Coding Teacher",
-      company: "Timedoor",
-      year: "2022 - present",
-      companyLink: "https://timedooracademy.com/",
-      desc: "- Teaching coding for kids and teenagers with Scratch, Javascript, and React by building games and websites, also teaching essential knowledge related to technology in general. - Making reports to parents about their kids' learning progress or needs.",
-    },
-    {
       title: "Freelance Front-End Developer",
       company: "Freelance",
-      year: "2022 - present",
+      year: "2022",
       companyLink: "https://faldi.xyz",
       desc: "- Developing some projects and websites from design to code. - Create pixel perfect and performance experiences.",
     },
-    {
-      title: "Part-Time Illustrator",
-      company: "MIUUU & BANANAAA Youtube Channels",
-      year: "2022 - present",
-      companyLink: "",
-      desc: "Creating illustrations based on given concepts and characters.",
-    },
-    {
-      title: "Teacher and Administrative Staff",
-      company: "English Made Fun School",
-      year: "2016 - 2019",
-      companyLink: "https://emfs.education",
-      desc: "- Teaching kids English as Second Language (ESL) while building their characters too. - Making reports to parents about their kids' learning progress or needs. - Preparing props and teaching materials each term. - Giving information about company profile and details to newcomers.",
-    },
+    
 
   ],
   resumeUrl:
@@ -102,7 +92,6 @@ const userData = {
     twitter: "https://twitter.com/flofernaur",
     linkedin: "https://www.linkedin.com/in/olfia-rachmi/",
     github: "https://github.com/floferno",
-    // facebook: "https://facebook.com/",
   },
 };
 
